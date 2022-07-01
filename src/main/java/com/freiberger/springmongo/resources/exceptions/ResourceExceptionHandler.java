279660355d83ee07239b49c95abc644cb140cd83
mfreiber <marcos.freiberger@capgemini.com>
2022-07-01 16:50:02 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/freiberger/springmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.freiberger.springmongo.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.freiberger.springmongo.services.exceptions.ObjectNotFoundException;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Não encontrado";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
