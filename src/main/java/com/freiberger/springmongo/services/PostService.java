f9cd769deabe1d91b7dafabdf883a623ef37e833
mfreiber <marcos.freiberger@capgemini.com>
2022-07-03 12:11:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/freiberger/springmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.freiberger.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.freiberger.springmongo.domain.Post;
+import com.freiberger.springmongo.repositories.PostRepository;
+import com.freiberger.springmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
