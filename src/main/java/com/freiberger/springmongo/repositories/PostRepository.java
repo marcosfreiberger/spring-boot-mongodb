448ecc0e29cb5a11b6c42e336e4c82ab05cee2cc
mfreiber <marcos.freiberger@capgemini.com>
2022-07-03 11:17:45 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/freiberger/springmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.freiberger.springmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.freiberger.springmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
