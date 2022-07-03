9adb47066cd586c1bf75685e346ddb8dbb61a362
mfreiber <marcos.freiberger@capgemini.com>
2022-07-03 12:29:14 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/freiberger/springmongo/domain/Post.java
+++ src/main/java/com/freiberger/springmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.freiberger.springmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
 import com.freiberger.springmongo.dto.AuthorDTO;
+import com.freiberger.springmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
