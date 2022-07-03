49f61d3b39a9a832e4c6a105bc2ea28ede0cf524
mfreiber <marcos.freiberger@capgemini.com>
2022-07-03 14:11:05 -0300

    Consulta simples com query methods

--- src/main/java/com/freiberger/springmongo/repositories/PostRepository.java
+++ src/main/java/com/freiberger/springmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.freiberger.springmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.freiberger.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
